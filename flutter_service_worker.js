2f3975df71ae714eae8cbe0199a1b795ca9066fc
hairilhumsani <hairilhumsani@gmail.com>
2022-06-10 03:04:06 +0000

    See https://github.com/hairilhumsani/hairilhumsani/commit/aa139e33dbef65b55133a4f0af070fda26264a7e from refs/heads/main

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,10 +3,10 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "version.json": "faddeee7b0e6f59275d0f6d9a243a536",
-"index.html": "92a8d3b1cd900a27b0dfde73e70efc56",
-"/": "92a8d3b1cd900a27b0dfde73e70efc56",
-"main.dart.js": "20d2dd41e29864b39923392cbf5ff8c8",
+  "version.json": "596d7af69624469ab49ed742285ce130",
+"index.html": "cfdd92e9966dd8e1d9fbe90e7333e0fb",
+"/": "cfdd92e9966dd8e1d9fbe90e7333e0fb",
+"main.dart.js": "8a91e1dfdd4412a2cfaf1a1b3c929007",
 "flutter.js": "0816e65a103ba8ba51b174eeeeb2cb67",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
@@ -15,7 +15,7 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "manifest.json": "d3931bc56085b9124f86e87330737cb8",
 "assets/AssetManifest.json": "2efbb41d7877d10aac9d091f58ccd7b9",
-"assets/NOTICES": "f0d1c704528694c44c6d34d264824e7f",
+"assets/NOTICES": "f4533614558fb6c142fb3756ac9228b0",
 "assets/FontManifest.json": "dc3d03800ccca4601324923c0b1d6d57",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "6d342eb68f170c97609e9da345464e5e",
 "assets/fonts/MaterialIcons-Regular.otf": "95db9098c58fd6db106f1116bae85a0b",
